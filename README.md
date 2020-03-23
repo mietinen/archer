f46ddf85f7866e128570579e57b9c242829318db
Aleksander Mietinen <aleksander@mietinen.net>
2020-03-23 04:35:58 +0100

    locale fix

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ hostname="archie"	# Machine hostname
 username="harry"	# Main user
 device="/dev/sda"	# Drive for install (something like /dev/nvme0n1 or /dev/sda)
 useefi=false		# Use EFI boot (true/false)
-language="en_US"	# Language for locale.conf
-locale="nb_NO"		# Time, numbers, messurement, etc. for locale.conf (safe to use same as language)
+language="en_GB"	# Language for locale.conf (en_GB for english with sane time format)
+locale="nb_NO"		# Numbers, messurement, etc. for locale.conf (safe to use same as language)
 keymap="no"		# Keymap (localectl list-keymaps)
 timezone="Europe/Oslo"	# Timezone (located in /usr/share/zoneinfo/../..)
 swapsize="3G"		# Size of swap partition (1500M, 8G, auto=MemTotal, 0=no swap)
--- archer.sh
+++ archer.sh
@@ -11,8 +11,8 @@ hostname="archie"	# Machine hostname
 username="harry"	# Main user
 device="/dev/sda"	# Drive for install (something like /dev/nvme0n1 or /dev/sda)
 useefi=false		# Use EFI boot (true/false)
-language="en_US"	# Language for locale.conf
-locale="nb_NO"		# Time, numbers, messurement, etc. for locale.conf (safe to use same as language)
+language="en_GB"	# Language for locale.conf (en_GB for english with sane time format)
+locale="nb_NO"		# Numbers, messurement, etc. for locale.conf (safe to use same as language)
 keymap="no"		# Keymap (localectl list-keymaps)
 timezone="Europe/Oslo"	# Timezone (located in /usr/share/zoneinfo/../..)
 swapsize="3G"		# Size of swap partition (1500M, 8G, auto=MemTotal, 0=no swap)
@@ -147,7 +147,7 @@ aichroot() {
 	printf "LC_NUMERIC=%s.UTF-8\n" "${locale}" >> /etc/locale.conf
 	printf "LC_PAPER=%s.UTF-8\n" "${locale}" >> /etc/locale.conf
 	printf "LC_TELEPHONE=%s.UTF-8\n" "${locale}" >> /etc/locale.conf
-	printf "LC_TIME=%s.UTF-8\n" "${locale}" >> /etc/locale.conf
+	printf "LC_TIME=%s.UTF-8\n" "${language}" >> /etc/locale.conf
 	showresult
 
 	# Setting timezone and adjtime
